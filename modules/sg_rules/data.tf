59b03f17e1241cb4ded8a42d197e88792e309e0d
skommana04 <skommana04@gmail.com>
2026-02-21 11:21:05 -0600

    a

--- modules/sg_rules/data.tf
+++ modules/sg_rules/data.tf
@@ -1,19 +1,19 @@
-data "aws_ssm_parameter" "frontend_sg_id" {
-  name = "/${var.project}/${var.env}/frontend_sg_id"
-}
+# data "aws_ssm_parameter" "frontend_sg_id" {
+#   name = "/${var.project}/${var.env}/frontend_sg_id"
+# }
 
-data "aws_ssm_parameter" "frontend-alb_sg_id" {
-  name = "/${var.project}/${var.env}/frontend-alb_sg_id"
-}
+# data "aws_ssm_parameter" "frontend-alb_sg_id" {
+#   name = "/${var.project}/${var.env}/frontend-alb_sg_id"
+# }
 
-data "aws_ssm_parameter" "backend-alb_sg_id" {
-  name = "/${var.project}/${var.env}/backend-alb_sg_id"
-}
+# data "aws_ssm_parameter" "backend-alb_sg_id" {
+#   name = "/${var.project}/${var.env}/backend-alb_sg_id"
+# }
 
-data "aws_ssm_parameter" "backend_sg_id" {
-  name = "/${var.project}/${var.env}/backend_sg_id"
-}
+# data "aws_ssm_parameter" "backend_sg_id" {
+#   name = "/${var.project}/${var.env}/backend_sg_id"
+# }
 
-data "aws_ssm_parameter" "bastion_sg_id" {
-  name = "/${var.project}/${var.env}/bastion_sg_id"
-}
+# data "aws_ssm_parameter" "bastion_sg_id" {
+#   name = "/${var.project}/${var.env}/bastion_sg_id"
+#}
