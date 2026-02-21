2a7ebec0f43ee2a819de17c43e96c1e41d365987
skommana04 <skommana04@gmail.com>
2026-02-21 11:19:22 -0600

    a

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -2,8 +2,8 @@ locals {
     ami_id = data.aws_ami.redhat.id
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
     public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
-    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
-    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
-    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
-    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
-}
+#     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+#     rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+#     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+#     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+# }
