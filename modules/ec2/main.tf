f96430552f05f74682db238b3080142899fd3e87
skommana04 <skommana04@gmail.com>
2026-02-21 11:18:26 -0600

    a

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -9,3 +9,15 @@ resource "aws_instance" "example" {
     Name = "${var.project}-${var.env}-bastion"
   }
 }
+
+# resource "aws_instance" "database" {
+#   ami           = local.ami_id
+#   instance_type = "t3.micro"
+#   vpc_security_group_ids = [ local.bastion_sg_id ]
+#   subnet_id = local.public_subnet_id
+
+#   tags = {
+#     Terraform = true
+#     Name = "${var.project}-${var.env}-bastion"
+#   }
+# }
